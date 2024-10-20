13aa324aa160b15433ba8df999fde416f79be60f
Kevin Solar <kevinleitesolar@hotmail.com>
2024-10-20 00:13:57 -0300

    fazendo com que todos as informacoes sejam enviadas para o console, com todas a informacoes preenchidas. feito com set do React para o ciclo interno

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,17 +1,11 @@
-import { useState } from 'react';
 import './CampoTexto.css'
 
 //Uma forma de criar a funcao eh com function, ou dessa forma, nao tem um jeito certo, mas eh bom ter um padrao.
-const CampoTexto = (props) => {/* Aqui pegamos o que esta sendo passado implicitamente com o props, para fazer a busca dos parametros que estamos passando para o nosso componente */
-
-   // let valor = ''
-
-   const [valor, setValor] = useState('Teste')
+const CampoTexto = (props) => {
+   /* Aqui pegamos o que esta sendo passado implicitamente com o props, para fazer a busca dos parametros que estamos passando para o nosso componente */
 
    const aoDigitar = (evento) => {
-      //valor = evento.target.value; -> Essa era a forma que estavamos usando antes, mas, a forma correta eh usando o State do React, entao ficara assim:
-      setValor(evento.target.value)
-      console.log(valor)
+      props.aoAlterado(evento.target.value)
    }
 
    return (
@@ -19,7 +13,7 @@ const CampoTexto = (props) => {/* Aqui pegamos o que esta sendo passado implicit
          <label>
             {props.label}  {/* Aqui faco a declaracao da minha label */}
          </label>
-         <input onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder} />
+         <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder} />
       </div>
    )
 
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,6 +1,7 @@
 import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
+import { useState } from 'react';
 //import CampoTexto from "./components/CampoTexto"; se usar index.js no componente criado, na importacao eu so preciso "puxar" pelo nome da pasta, pq por padrao ele sempre busca o index.js
 import "./Formulario.css";
 
@@ -16,28 +17,41 @@ const Formulario = () => {
       'Inovacao e Gestao'
    ]
 
+   const [nome, setNome] = useState('')
+   const [cargo, setCargo] = useState('')
+   const [imagem, setImagem] = useState('')
+
    const aoSalvar = (evento) => {
       evento.preventDefault()
       alert('Foi enviado!')
    }
-   
-	return (
-		<section className="formulario">
-			<form onSubmit={aoSalvar}>
+
+   return (
+      <section className="formulario">
+         <form onSubmit={aoSalvar}>
             <h2>Preencha os dados para criar o card do colaborador</h2>
-				{/* Passo como parametro uma label com o que desejo imprimir, nesse caso eh label="Nome", entao preciso declarar isso no meu componente agora. */}
-				<CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-				<CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-				<CampoTexto obrigatorio={false} label="Imagem" placeholder="Digite o endereco da imagem" />
+            {/* Passo como parametro uma label com o que desejo imprimir, nesse caso eh label="Nome", entao preciso declarar isso no meu componente agora. */}
+            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"
+               valor={nome}
+               aoAlterado={valor => setNome(valor)} //Para alterar o valor que recebemos do evento "valor"
+            />
+            <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"
+               valor={cargo}
+               aoAlterado={valor => setCargo(valor)}
+            />
+            <CampoTexto obrigatorio={false} label="Imagem" placeholder="Digite o endereco da imagem" 
+               valor={imagem}
+               aoAlterado={valor => setImagem(valor)}
+            />
             <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
-            
+
             {/* <Botao texto="criar card" /> //Esse seria o jeito certo de fazer, mas vamos fazer como se fosse um html para demonstrar varias maneiras de fazer! */}
             <Botao>
                criar card
             </Botao>
-			</form>
-		</section>
-	);
+         </form>
+      </section>
+   );
 };
 
 export default Formulario;
