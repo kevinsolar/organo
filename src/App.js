fe5d969da4a9fc6156472c7bf07560dc2f75a829
Kevin BTA <kevinleitesolar@hotmail.com>
2024-10-21 17:41:12 -0300

    dando vida para nosso componente time, criando uma lista que recebe todos as categorias e nela as cores e assim criando o componente com props

--- src/App.js
+++ src/App.js
@@ -13,6 +13,44 @@ function App() {
       setColaboradores([...colaboradores, colaborador])
    }
 
+   const times = [
+      {
+         nome: 'Programacao',
+         corPrimaria: '#57C278',
+         corSecundaria: '#D9F7E9',
+      },
+      {
+         nome: 'Front-End',
+         corPrimaria: '#82CFFA',
+         corSecundaria: '#E8F8FF',
+      },
+      {
+         nome: 'Data Science',
+         corPrimaria: '#A6D157',
+         corSecundaria: '#F0F8E2',
+      },
+      {
+         nome: 'Devops',
+         corPrimaria: '#E06B69',
+         corSecundaria: '#FDE7E8',
+      },
+      {
+         nome: 'UX e Design',
+         corPrimaria: '#DB6EBF',
+         corSecundaria: '#FAE9F5',
+      },
+      {
+         nome: 'Mobile',
+         corPrimaria: '#FFBA05',
+         corSecundaria: '#FFF5D9',
+      },
+      {
+         nome: 'Inovacao e Gestao',
+         corPrimaria: '#FF8A29',
+         corSecundaria: '#FFEEDF',
+      },
+   ]
+
 	return (
 		<div className="App">
          <Banner />
@@ -20,10 +58,8 @@ function App() {
          {/* Vamos com nosso formulario ouvir um pedido de cadastro para criar um card com
             nossos colaboradores. */}
          
-         <Time nome="Programação" />
-         <Time nome="Front-End" />
-         <Time nome="Data Science" />
-         <Time nome="Devops" />
+         {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+         
 		</div>
 	);
 }
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,11 @@
 import './Time.css';
 
+//O nome desse tipo de componente é Dumb, pois ele é mais estático, só recebe o texto para ser exibido.
+
 const Time = (props) => {
    return (
-      <section className='time'>
-         <h3>{props.nome}</h3>
+      <section className='time' style={{ backgroundColor: props.corSecundaria }}>
+         <h3 style={{ color: props.corPrimaria, borderColor: props.corPrimaria }}>{props.nome}</h3>
       </section>
    )
 }
